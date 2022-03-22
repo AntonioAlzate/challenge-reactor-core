b50b79a10276c079633e4e4b189b539ed33afdcc
Antonio Alzate Naranjo <antonioalzate7@gmail.com>
2022-03-22 13:50:35 -0500

    Ajustes en Controlador.

--- src/main/java/com/example/demo/controllers/PlayerController.java
+++ src/main/java/com/example/demo/controllers/PlayerController.java
@@ -3,10 +3,7 @@ package com.example.demo.controllers;
 import com.example.demo.collections.Player;
 import com.example.demo.services.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 @RestController
@@ -16,12 +13,12 @@ public class PlayerController {
     @Autowired
     private PlayerService service;
 
-    @GetMapping("obtener/mayores34/{club}")
-    public Flux<Player> obtenerJugadoresMayores34DelClub(@PathVariable("club") String club) {
+    @GetMapping("mayores-34")
+    public Flux<Player> obtenerJugadoresMayores34DelClub(@RequestParam("club") String club) {
         return service.obtenerJugadoresMayores34DelClub(club);
     }
 
-    @GetMapping("obtener/ranking-por-pais")
+    @GetMapping("ranking-por-pais")
     public Flux<Player> obtenerMayorRankingCadaPais() {
         return service.obtenerMayorRankingCadaPais();
     }
